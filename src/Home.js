93e37db734eb98f25d32e01eacee9019f7277502
Andrew Pryshchepa <vinglfm@gmail.com>
2016-07-10 21:39:39 +0300

    Added an example of routing

--- src/Home.js
+++ src/Home.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 
-export default class App extends Component {
+export default class Home extends Component {
   render() {
     return (
       <div className="jumbotron">
--- src/index.js
+++ src/index.js
@@ -1,5 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import Home from './Home';
+import About from './views/about/About'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+class App extends React.Component {
+  render() {
+    var Child;
+
+    switch(this.props.route) {
+      case 'about':
+        Child = About;
+      break;
+      default:
+        Child = Home;
+    }
+    return (
+      <div>
+        <Child/>
+      </div>
+    );
+  }
+}
+function render() {
+  var route = window.location.hash.substr(1);
+  console.log(route);
+  ReactDOM.render(<App route={route}/>, document.getElementById('root'));
+}
+window.addEventListener('hashchange', render);
+render();
+
+// ReactDOM.render(<App />, document.getElementById('root'));
