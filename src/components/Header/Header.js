ee226b75e63d5ae5ea3295e444b777c3109fb2d9
Andrew Pryshchepa <vinglfm@gmail.com>
2016-07-20 14:01:17 +0300

    Adding import rule for css file to eslint config

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import {Link, IndexLink} from 'react-router';
-// import styles from './styles.module.css';
+import styles from './styles.module.css';
 
 export default class Header extends React.Component {
   render() {
